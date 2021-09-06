f62af1efc5b82aeb673b8b4ceebb9831e7df698a
n4to4 <n4to4k@gmail.com>
2021-09-06 18:03:58 +0900

    demo app

--- series/02_hyper/src/main.rs
+++ series/02_hyper/src/main.rs
@@ -18,3 +18,56 @@ async fn main() {
         eprintln!("server error: {}", e);
     }
 }
+
+use hyper::service::Service;
+use std::future::Ready;
+use std::sync::{atomic::AtomicUsize, Arc};
+use std::task::Poll;
+
+struct DemoApp {
+    counter: Arc<AtomicUsize>,
+}
+
+impl Service<Request<Body>> for DemoApp {
+    type Response = Response<Body>;
+    type Error = hyper::http::Error;
+    type Future = Ready<Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut std::task::Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, _req: Request<Body>) -> Self::Future {
+        let counter = self
+            .counter
+            .fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+        let res = Response::builder()
+            .status(200)
+            .header("Context-Type", "text/plain; charset=utf-8")
+            .body(format!("Counter is at: {}", counter).into());
+        std::future::ready(res)
+    }
+}
+
+use hyper::server::conn::AddrStream;
+
+struct DemoAppFactory {
+    counter: Arc<AtomicUsize>,
+}
+
+impl Service<&AddrStream> for DemoAppFactory {
+    type Response = DemoApp;
+    type Error = Infallible;
+    type Future = Ready<Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut std::task::Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, conn: &AddrStream) -> Self::Future {
+        println!("Accepting a new connection from {:?}", conn);
+        std::future::ready(Ok(DemoApp {
+            counter: Arc::clone(&self.counter),
+        }))
+    }
+}
