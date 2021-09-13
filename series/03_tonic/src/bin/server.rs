1f6ad2ade2f496d5ef5a3abb0e98f67325de664f
n4to4 <n4to4k@gmail.com>
2021-09-13 18:42:35 +0900

    server2

--- series/03_tonic/src/bin/server.rs
+++ series/03_tonic/src/bin/server.rs
@@ -2,6 +2,9 @@ use tonic::async_trait;
 use tonic_example::echo_server::{Echo, EchoServer};
 use tonic_example::{EchoReply, EchoRequest};
 
+use hyper::service::make_service_fn;
+use std::convert::Infallible;
+
 pub struct MyEcho;
 
 #[async_trait]
@@ -17,13 +20,35 @@ impl Echo for MyEcho {
 }
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn _main() -> anyhow::Result<()> {
     let addr = ([0, 0, 0, 0], 3000).into();
 
     tonic::transport::Server::builder()
         .add_service(EchoServer::new(MyEcho))
         .serve(addr)
         .await?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let addr = ([0, 0, 0, 0], 3000).into();
+
+    let grpc_service = tonic::transport::Server::builder()
+        .add_service(EchoServer::new(MyEcho))
+        .into_service();
+
+    let make_grpc_service = make_service_fn(move |_conn| {
+        let grpc_service = grpc_service.clone();
+        async { Ok::<_, Infallible>(grpc_service) }
+    });
+
+    let server = hyper::Server::bind(&addr).serve(make_grpc_service);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 
     Ok(())
 }
